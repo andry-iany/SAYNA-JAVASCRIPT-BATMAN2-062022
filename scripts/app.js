b7c400261ac98762201dd95c785e06b17d3b43fc
Andry <andrysitrakiniainasoa@gmail.com>
2022-06-08 07:09:21 +0300

    Refactor animation

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,17 @@
-const eltsToAnimate = document.querySelectorAll("[data-animation='fade-in']");
-const threshold = 1;
+const eltsToAnimate = document.querySelectorAll(".fade-in-hidden");
 const observer = new IntersectionObserver(handleIntersecting);
+
+// register observer to trigger animation when the element is visible
 eltsToAnimate.forEach((elt) => observer.observe(elt));
 
 function handleIntersecting(entries) {
   entries.forEach(
     (entry) => {
-      if (entry.isIntersecting) entry.target.classList.add("fade-in");
+      if (entry.isIntersecting) {
+        entry.target.classList.replace("fade-in-hidden", "fade-in-shown");
+        observer.unobserve(entry.target); // unobserve the element once it's been animated
+      }
     },
-    { root: null, threshold }
+    { root: null }
   );
 }
